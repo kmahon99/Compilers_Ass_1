786f9d1b4533ba50d90ee7b9b8c35fc1bf1328a0
Kevin Mahon <kev@false>
2016-02-08 18:05:47 +0000

    Began lex analysis

--- Lexer.py
+++ Lexer.py
@@ -3,14 +3,26 @@ import DataStream as Stream
 class Lexer:
     def __init__(self, filename):
         self.filename = filename
+        self.stream = Stream.Source(self.filename)
 
     def Driver(self):
-        stream = Stream.Source(self.filename)
-        if stream == None:
+        if self.stream == None:
             print("Error")
         else:
-            c = stream.nextChar()
-            while c != "END":
-                print(c)
-                c = stream.nextChar()
+            c = self.stream.nextChar()
+            return c
 
+
+    def GetAnalysis(self):
+        c = self.Driver()
+        while c != "END":
+            if ord(c) >= 65 and ord(c) <= 90:
+                self.checkStatic(c)
+
+    def checkStatic(self, startchar):
+        item = startchar
+        c = self.Driver()
+        while (ord(c) >= 48 and ord(c) <= 57) and (ord(c) != )
+
+l = Lexer("test.txt")
+l.Driver()
---
+++ Token.py
@@ -0,0 +1,4 @@
+class Token:
+    def __init__(self, value, id):
+        self.value = value
+        self.id = id
