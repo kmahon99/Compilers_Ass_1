92304e47692fb75278d78396feb2992aa9300a0b
Kevin Mahon <13379741@ucdconnect.ie>
2016-02-14 21:14:40 +0000

    Fixed regex issues with Lexer

--- Lexer.py
+++ Lexer.py
@@ -3,7 +3,8 @@ import SymbolTable
 
 class Lexer:
     def __init__(self, filename):
-        self.delims = [' ', '\n', '\t', '\r', ';']
+        self.delims = [' ', '\n', '\t', '\r']
+        self.singles = ['(',')',';']
         self.table = SymbolTable.SymbolTable("ReservedSymbols.txt")
         self.state = None
         self.filename = filename
@@ -14,38 +15,45 @@ class Lexer:
         while self.state != "EOF":
             oldstate = self.state
             char = self.getNextChar()
-            if oldstate != self.state and (oldstate == "IDENT" or oldstate == "STATIC"):
-                print("CHECKING:",ident)
-                ident = ""
-                id = self.table.getID()
-                print("Got ID: ", id)
-            if self.state == "IDENT":
-                ident += char
-                self.table.processChar(char,True)
-            elif self.state == "STATIC":
-                ident += char
-                self.table.processChar(char, False)
+            if char == None: return
+            #ident += char
+            if oldstate != self.state:
+                print(self.state)
+                #ident = ""
+            print(char)
 
+    # gets the next character and changes the Lexer's state based on it's type
     def getNextChar(self):
         char = self.stream.nextChar()
         if char == '': self.state = "EOF"
-        elif self.state == "STRING" and ord(char) == 126:
-            self.state = "TILDE"
-            return
-        elif self.state == "TILDE":
+        elif ord(char) == 126 and self.state == "STRING": char = self.stream.nextChar() # tilde in string, get the next char regardless
+        elif ord(char) != 34 and self.state == "STRING": self.state == "STRING"
+        elif ord(char) == 34 and self.state == "STRING":
+            self.state = "STRING_END"
+            char = self.stream.nextChar()
+        elif ord(char) == 34 and self.state != "STRING":
             self.state = "STRING"
-            return char
-        elif self.state == "STRING" and ord(char) != 34: return char# we're inside a string, this doesn't go anywhere
-        elif ord(char) == 34 and self.state == "STRING": # this is the ending quotes for a string we've traversed
-            self.state = None
-            self.getNextChar()
-        elif ord(char) == 34 and self.state != "STRING": self.state = "STRING"
-        elif ord(char) in range(97,122) and self.state != "STATIC": self.state = "IDENT"
-        elif ord(char) in range(97,122) and self.state == "STATIC": self.state = "STATIC"
-        elif ord(char) in range(65, 90): self.state = "STATIC"
-        elif ord(char) in range(48,57): self.state = "INT"
+            char = self.stream.nextChar()
+        elif ord(char) in range(65,91) and self.state != "IDENT": self.state = "STATIC"
+        elif ord(char) in range(65,91) and self.state == "IDENT": self.state = "IDENT"
+        elif ord(char) in range(97,123) and self.state != "STATIC": self.state = "IDENT"
+        elif ord(char) in range(97,123) and self.state == "STATIC": self.state = "STATIC"
+        elif ord(char) in range(48,58): self.state = "INT"
         elif char in self.delims: self.state = "DELIM"
+        elif char in self.singles: self.state = "SINGLE"
+        else: self.state = "UNKNOWN"
         return char
+
+    # helper function to prevent oversized int values being created
+    def getValidInt(self,string_value):
+        max_int = "65535"
+        if len(string_value) > 5: return -1
+        elif len(string_value) == 5:
+            for i in range(0,5):
+                if int(string_value[i]) < int(max_int[i]): return int(string_value)
+                elif int(string_value[i]) > int(max_int[i]): return -1
+        return int(string_value)
+
 
 l = Lexer("test.txt")
 l.Driver()
